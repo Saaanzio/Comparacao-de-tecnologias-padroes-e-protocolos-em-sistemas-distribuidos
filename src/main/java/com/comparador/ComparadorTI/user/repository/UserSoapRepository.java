d3c0fe9d885da1a4d1bd9ceed9008cd902570bd8
Saaanzio <rafaelsanziosilva22@gmail.com>
2025-11-08 18:40:13 -0300

    Removed soap

--- src/main/java/com/comparador/ComparadorTI/user/repository/UserSoapRepository.java
+++
@@ -1,9 +0,0 @@
-package com.comparador.ComparadorTI.user.repository;
-
-
-import com.projetoti.spring.soap.api.usersoap.UserRequest;
-import com.projetoti.spring.soap.api.usersoap.UserResponse;
-
-public interface UserSoapRepository {
-    UserResponse createUser(UserRequest userRequest);
-}
--- src/main/java/com/comparador/ComparadorTI/user/service/UserSoapService.java
+++
@@ -1,9 +0,0 @@
-package com.comparador.ComparadorTI.user.service;
-
-
-import com.projetoti.spring.soap.api.usersoap.UserRequest;
-import com.projetoti.spring.soap.api.usersoap.UserResponse;
-
-public interface UserSoapService {
-    UserResponse createUser(UserRequest userRequest);
-}
